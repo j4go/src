235a585e759c397bcc4827c0a20cffdbeedf853e
Molock <yianny@163.com>
2018-08-22 17:25:31 +0800

    jjjjj

---
+++ ginstudy/templates/vue/helloworld_didi/index.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: "#didi-navigator",
+    data: {
+        tabs : [
+            {text: '巴士'},
+            {text: '快车'},
+            {text: '专车'},
+            {text: '出租车'},
+            {text: '代驾'}
+        ]
+    }
+})
